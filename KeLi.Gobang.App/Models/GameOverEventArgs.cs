a548617f1e71b061ba443ecb6f7be517646b3111
KeLi <kelicto@protonmail.com>
2021-07-18 21:15:07 +0800

    Updates code.

---
+++ KeLi.Gobang.App/Models/GameOverEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KeLi.Gobang.App.Models
+{
+    public class GameOverEventArgs : EventArgs
+    {
+        public GameOverEventArgs(int color)
+        {
+            Color = color;
+        }
+
+        public int Color { get; set; }
+    }
+}
---
+++ KeLi.Gobang.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+using KeLi.Gobang.App.Forms;
+
+namespace KeLi.Gobang.App
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
